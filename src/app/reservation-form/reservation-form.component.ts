cda9837f152eec83ae628cca35a8d704c5145168
arupay <arupay11374@gmail.com>
2024-06-29 08:58:15 -0400

    form group validations, dependency injections, and lifecycel hook implementation

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,6 +6,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css',
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({});
+
+  //Dependency injection: Form Builder instance is injected into the component
+  //private to ensure only accesible within this component. ENCAPSULATION
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    //lifecycle hook used to initialize the reservation form group using the formBuilderservice.
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      guestName: ['', Validators.required],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log('valid');
+    }
+  }
 }
